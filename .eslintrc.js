d21c811771f0c4e8285c5ccb976b246d06f4ef21
Qaisar Irfan <qaisar.irfan.2888@gmail.com>
2020-09-24 00:57:31 +0500

    setup for android e2e

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,15 @@ module.exports = {
       },
     ],
   },
-  plugins: ["prettier"],
+  plugins: ["prettier", "detox"],
+  overrides: [
+    {
+      files: ["*.e2e.js"],
+      env: {
+        "detox/detox": true,
+        jest: true,
+        "jest/globals": true,
+      },
+    },
+  ],
 }
--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -8,20 +8,27 @@ describe("Example", () => {
   })
 
   it("should show hello screen after tap", async () => {
+    await waitFor(element(by.id('reverse_button'))).toBeVisible().whileElement(by.id('home_screen')).scroll(50, 'down');
     await element(by.id("reverse_button")).tap()
-    await expect(element(by.text("Reverse Currencies"))).toBeVisible()
   })
 
   it("should show hello screen after tap", async () => {
     await expect(element(by.id("conversion_input_1"))).toBeVisible()
     await element(by.id("conversion_input_1")).typeText("I also typed this")
-    await element(by.id("conversion_input_1")).tapReturnKey()
     await element(by.id("conversion_input_1")).clearText()
   })
 
-  it("should chanage theme", async () => {
+  it("should chanage the theme to yellow", async () => {
     await element(by.id("options_screen_button")).tap()
     await element(by.id("themes")).tap()
+    await expect(element(by.text("Theme 3"))).toBeVisible()
     await element(by.id("theme-3")).tap()
+  })
+
+  it("should chanage the theme to green", async () => {
+    await element(by.id("options_screen_button")).tap()
+    await element(by.id("themes")).tap()
+    await expect(element(by.text("Theme 1"))).toBeVisible()
+    await element(by.id("theme-1")).tap()
   })
 })
